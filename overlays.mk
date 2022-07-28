60eb830f2ce2a704a179680a95a92f0bf090cea3
AndroBuddy <anirudhchimpidi8@gmail.com>
2022-07-28 08:10:46 +0530

    fonts: Import Fluid sans font * all credits to project fluid * font by AndroBuddy
    Co-authored-by: immat0x1 <fl3ksmashina@gmail.com>
    Co-authored-by: Arseniy Graur <argraur@aosip.dev>
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- overlays.mk
+++ overlays.mk
@@ -55,6 +55,7 @@ PRODUCT_PACKAGES += \
     FontOneplusSansOverlay \
     FontOneplusSlateSource \
     FontOppoSansOverlay \
+    FontFluidSansOverlay \
     FontRosemarySourceOverlay \
     FontSamsungOneSourceOverlay \
     FontSonySketchSourceOverlay \
